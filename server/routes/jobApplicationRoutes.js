812951eb79c1c978d44034974e71659010376171
mohit <singm2698@gmail.com>
2025-07-05 20:50:45 +0530

    new server

--- server/routes/jobApplicationRoutes.js
+++ server/routes/jobApplicationRoutes.js
@@ -12,14 +12,14 @@ const {
 } = require("../controllers/jobApplicationController");
 const adminAuth = require("../middleware/adminAuth");
 
-// Public routes (for job applications)
+// Public routes (for job applications) - These should be first
 router.post("/", createApplication);
 router.get("/recent", getRecentApplications);
 
-// Admin routes (protected)
-router.get("/", adminAuth, getAllApplications);
+// Admin routes (protected) - These should be after public routes
 router.get("/stats", adminAuth, getApplicationStats);
 router.get("/job/:jobId", adminAuth, getApplicationsByJob);
+router.get("/", adminAuth, getAllApplications);
 router.get("/:id", adminAuth, getApplication);
 router.put("/:id/status", adminAuth, updateApplicationStatus);
 router.delete("/:id", adminAuth, deleteApplication);
